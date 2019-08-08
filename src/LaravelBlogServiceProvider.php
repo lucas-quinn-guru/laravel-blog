1205f7039f488dc33833413641fde70452bab869
Marcellus Barrus <mbarrus73@gmail.com>
2019-08-08 16:08:34 -0600

    Initial commit

---
+++ src/LaravelBlogServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace LucasQuinnGuru\LaravelBlog;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelBlogServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/laravel_user.php',
+            'laravel_blog'
+        );
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom( __DIR__ . '/../routes/web.php' );
+        $this->loadMigrationsFrom(__DIR__.'/../database//migrations');
+
+        $this->publishes([
+            __DIR__ . '/../config/laravel_blog.php' => config_path('laravel_blog.php'),
+        ], 'laravel-blog-config');
+
+        $this->publishes([
+            __DIR__ . '/../resources/assets' => public_path('vendor/lucas-quinn-guru/blog'),
+        ], 'assets');
+    }
+}
