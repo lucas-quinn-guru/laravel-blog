4238aaa7ad8293db9f332241bf6f435f7aef09a5
Marcellus Barrus <mbarrus73@gmail.com>
2019-08-08 19:53:39 -0600

    updating some coding styles

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-Route::group(['namespace' => 'LucasQuinnGuru\LaravelBlog\Controllers'], function()
-{
+Route::group(['namespace' => 'LucasQuinnGuru\LaravelBlog\Controllers'], function () {
     //Route::get('/blog', ['uses' => 'PostController@index']);
 
     Route::resource('blog', 'PostController');
--- src/LaravelBlogServiceProvider.php
+++ src/LaravelBlogServiceProvider.php
@@ -26,7 +26,7 @@ class LaravelBlogServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadRoutesFrom( __DIR__ . '/../routes/web.php' );
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         $this->loadMigrationsFrom(__DIR__.'/../database//migrations');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-blog');
 
