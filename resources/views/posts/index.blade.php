526be5440ff1cfcb616d5c0901e654447b1c2fbd
Marcellus Barrus <mbarrus73@gmail.com>
2019-10-16 16:01:03 -0600

    -Making it so that html is parsed correctly from the post pages

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,7 +8,7 @@
                 <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
                 <div class="card-body">
                     <h2 class="card-title">{{ $post->title }}</h2>
-                    <p class="card-text">{{ $post->body }}</p>
+                    <p class="card-text">{!! $post->body !!}</p>
                     <a href="{{ route('posts.show', $post->id ) }}" class="btn btn-primary">Read More &rarr;</a>
                 </div>
             </div>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,7 +9,7 @@
     <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
     <div class="card-body">
         <h2 class="card-title">{{ $post->title }}</h2>
-        <p class="card-text">{{ $post->body }}</p>
+        <p class="card-text">{!! $post->body !!}</p>
     </div>
 </div>
 
