c8ae24b24f7aa1563d51bbfc5eb82a2c9a355a02
Marcellus Barrus <mbarrus73@gmail.com>
2019-10-16 11:43:57 -0600

    -Adding dynamic post->id to breadcrumb

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -5,12 +5,12 @@ Breadcrumbs::for('sitetronic-posts-admin-index', function ($trail) {
     $trail->push('Posts', route('admin.posts.index'));
 });
 
-Breadcrumbs::for('sitetronic-posts-admin-edit', function ($trail) {
+Breadcrumbs::for('sitetronic-posts-admin-edit', function ($trail, $post) {
     $trail->parent('sitetronic-posts-admin-index');
-    $trail->push('Posts', route('admin.posts.edit'));
+    $trail->push('Posts', route('admin.posts.edit', $post->id));
 });
 
-Breadcrumbs::for('sitetronic-posts-admin-show', function ($trail) {
+Breadcrumbs::for('sitetronic-posts-admin-show', function ($trail, $post) {
     $trail->parent('sitetronic-posts-admin-index');
-    $trail->push('Posts', route('admin.posts.show'));
+    $trail->push('Posts', route('admin.posts.show', $post->id));
 });
